018daee7c5819cc5344b31ae0ea3da3e29da0101
Josh-Payne <snare117@gmail.com>
2017-06-14 22:21:32 -0500

    Initial Commit

---
+++ demo-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo-app
+//
+//  Created by Josh Payne on 6/14/17.
+//  Copyright © 2017 R|TRIBE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
