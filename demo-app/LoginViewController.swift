2be0cb0a099df51f78621803356831976921597c
Josh-Payne <snare117@gmail.com>
2017-06-15 19:35:23 -0500

    first commit

---
+++ demo-app/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  demo-app
+//
+//  Created by Josh Payne on 6/14/17.
+//  Copyright © 2017 R|TRIBE. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1)
+    }
+}
--- demo-app/ViewController.swift
+++ demo-app/ViewController.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(logout))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func logout() {
+        present(LoginViewController, animated: true, completion: nil)
     }
-
-
+    
 }
 
